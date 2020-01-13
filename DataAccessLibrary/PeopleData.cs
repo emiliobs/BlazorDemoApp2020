911478e6a2be9505d0f852e78d13211f0f9d27db
emilio barrera <barrera_emilio@hotmail.com>
2020-01-13 22:08:52 +0000

    Add project files.

---
+++ DataAccessLibrary/PeopleData.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class PeopleData : IPeopleData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public PeopleData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<List<PersonModel>> GetPeople()
+        {
+            var sql = "Select * from People";
+
+            return _db.LoadData<PersonModel, dynamic>(sql, new { });
+        }
+
+        public Task InsertPerson(PersonModel person)
+        {
+            var sql = @"Insert into People(FirstName, LastName, EmailAddress) values (@FirstName, @LastName, @EmailAddress);";
+
+            return _db.SaveData(sql, person);
+
+        }
+
+    }
+}
---
+++ DataAccessLibrary/SqlDataAccess.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _configuration;
+
+        public string ConnectionStringName { get; set; } = "Default";
+        public SqlDataAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameter)
+        {
+            var connectioString = _configuration.GetConnectionString(ConnectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectioString))
+            {
+                var data = await connection.QueryAsync<T>(sql, parameter);
+
+                return data.ToList();
+            }
+        }
+
+        public async Task SaveData<T>(string sql, T parameter)
+        {
+            var connectioString = _configuration.GetConnectionString(ConnectionStringName);
+
+            using (var connection = new SqlConnection(connectioString))
+            {
+                await connection.ExecuteAsync(sql, parameter);
+            }
+        }
+
+
+    }
+}
